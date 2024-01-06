c1af51e8bf7e64cae187adb748e43dcbcca38995
Михаил Пигалов <pigalovmihail@yandex.ru>
2024-01-06 14:55:49 +0300

    mvvm

--- SignalMessenger/ViewModels/LoginViewModel.cs
+++ SignalMessenger/ViewModels/LoginViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 
 namespace SignalMessenger.ViewModels
 {
@@ -11,16 +12,42 @@ namespace SignalMessenger.ViewModels
 	{
         private IRegionManager _regionManager;
 
+        private string login;
+        public string Login
+        {
+            get => login;
+            set
+            {
+                SetProperty(ref login, value);
+                LoginCommand.RaiseCanExecuteChanged();
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get => password;
+            set
+            {
+                SetProperty(ref password, value);
+                LoginCommand.RaiseCanExecuteChanged();
+            }
+        }
         public DelegateCommand LoginCommand { get; }
 
         public LoginViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
 
-            LoginCommand = new DelegateCommand(Login);
+            LoginCommand = new DelegateCommand(loginCommand, CanLogin);
         }
 
-        private void Login()
+        bool CanLogin()
+        {
+            if(!string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password)) return true;
+            else return false;
+        }
+
+        private void loginCommand()
         {
             _regionManager.RequestNavigate("MainRegion","WorkplaceView");
         }
